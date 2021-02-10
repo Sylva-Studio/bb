cb70ff010f22f1370bc53c2f3b941016e9612195
sylva <okekesylvaline@gmail.com>
2021-02-10 20:28:00 +0100

    made corrections on category folder

--- src/pages/Categories/index.js
+++ src/pages/Categories/index.js
@@ -2,27 +2,28 @@ import React from 'react'
 import Slider from '../../component/Slider'
 function index() {
   return (
-    <div >
-      <div className=" item category_item">
+    <div className="">
+      
+      <div className="category_item">
+      <h4> category</h4>
+      </div>
+      <div className="category_item">
+      <h4> category</h4>
+      </div>
+      <div className="category_item">
+      <h4> category</h4>
+      </div><div className="category_item">
+      <h4> category</h4>
+      </div><div className="category_item">
+      <h4> category</h4>
+      </div><div className="category_item">
+      <h4> category</h4>
+      </div>
+      <div className="category_item">
+      <h4> category</h4>
+      </div>
+      <div className=" item category_item1">
       <Slider />
-      </div>
-      <div className="category_item">
-      <h4> category</h4>
-      </div>
-      <div className="category_item">
-      <h4> category</h4>
-      </div>
-      <div className="category_item">
-      <h4> category</h4>
-      </div><div className="category_item">
-      <h4> category</h4>
-      </div><div className="category_item">
-      <h4> category</h4>
-      </div><div className="category_item">
-      <h4> category</h4>
-      </div>
-      <div className="category_item">
-      <h4> category</h4>
       </div>
     </div>
   )
